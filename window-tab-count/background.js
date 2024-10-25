170bb3335f5315995693580293608c2343ee96e6
Rahman Khandakar <74515048+CrymsonShadows@users.noreply.github.com>
2024-10-24 19:49:58 -0500

    Only query for tabs when extension installed and changed focus

--- window-tab-count/background.js
+++ window-tab-count/background.js
@@ -1,6 +1,9 @@
+let numTabs = 0;
+
 chrome.runtime.onInstalled.addListener(async () => {
+  numTabs = await getNumTabs();
   chrome.action.setBadgeText({
-    text: await getNumTabs(),
+    text: numTabs.toString(),
   });
 });
 
@@ -8,29 +11,30 @@ const queryOptions = { windowId: chrome.windows.WINDOW_ID_CURRENT };
 
 async function getNumTabs() {
   const tabs = await chrome.tabs.query(queryOptions);
-  return tabs.length.toString();
+  return tabs.length;
 }
 
 chrome.windows.onFocusChanged.addListener(async () => {
+  numTabs = await getNumTabs();
   chrome.action.setBadgeText({
-    text: await getNumTabs(),
+    text: numTabs.toString(),
   });
 });
 
 chrome.tabs.onCreated.addListener(async () => {
   chrome.action.setBadgeText({
-    text: await getNumTabs(),
+    text: (++numTabs).toString(),
   });
 });
 
-chrome.tabs.onUpdated.addListener(async () => {
-  chrome.action.setBadgeText({
-    text: await getNumTabs(),
-  });
-});
+// chrome.tabs.onUpdated.addListener(async () => {
+//   chrome.action.setBadgeText({
+//     text: await getNumTabs(),
+//   });
+// });
 
 chrome.tabs.onRemoved.addListener(async () => {
   chrome.action.setBadgeText({
-    text: await getNumTabs(),
+    text: (--numTabs).toString(),
   });
 });
