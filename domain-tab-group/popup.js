51bf6d30c161414296dbc48bee63d873cf020a94
Rahman Khandakar <74515048+CrymsonShadows@users.noreply.github.com>
2024-10-30 14:54:00 -0500

    Create a popup to group tabs by domain in mass

--- domain-tab-group/popup.js
+++ domain-tab-group/popup.js
@@ -1,11 +1,29 @@
-let domains = new Set();
-const queryOptions = { windowId: windows.WINDOW_ID_CURRENT };
-const tabs = await chrome.tabs.query(queryOptions);
+let domains = new Map();
+const queryOptions = { windowId: chrome.windows.WINDOW_ID_CURRENT };
+// const tabs = await chrome.tabs.query(queryOptions);
 const button = document.querySelector("button");
-button.addEventListener("click", async () => {
-  const tabIds = tabs.map(({ id }) => id);
-  if (tabIds.length) {
-    const group = await chrome.tabs.group({ tabIds });
-    await chrome.tabGroups.update(group, { title: "DOCS" });
+button.addEventListener("click", groupTabByDomain);
+
+async function groupTabByDomain() {
+  const tabs = await chrome.tabs.query(queryOptions);
+  for (const tab of tabs) {
+    let domain = new URL(tab.url).hostname;
+    if (!domains.get(domain)) {
+      domains.set(domain, [tab.id]);
+      continue;
+    }
+
+    domains.get(domain).push(tab.id);
   }
-});
+  //   console.log(domains);
+  for (let [domainName, tabIDs] of domains) {
+    // console.log(`tabIDs of ${domainName}: ${tabIDs}`);
+    if (tabIDs.length) {
+      const groupID = await chrome.tabs.group({ tabIds: tabIDs });
+      await chrome.tabGroups.update(groupID, {
+        title: domainName,
+        collapsed: true,
+      });
+    }
+  }
+}
